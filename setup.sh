2a0e46dae8cfa083772fba24cf28a355df23beba
nivdon <dnivedita76@gmail.com>
2021-12-24 04:43:16 +0000

    Adding codeDeploy

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo yum install java-11-amazon-corretto-headless
-java -jar spring-petclinic*.jar > /dev/null 2 > /dev/null < /dev/null
+#java -jar spring-petclinic*.jar > /dev/null 2 > /dev/null < /dev/null
 #sh ./run.sh
 exit 0
