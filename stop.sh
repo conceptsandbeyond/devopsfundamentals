ed901b3120bbd1d853f00fa62b2b367d7178f779
nivdon <dnivedita76@gmail.com>
2021-12-30 22:07:00 +0000

    modified the file

--- stop.sh
+++ stop.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-cd /home/ec2-user/student3
-echo "running the Java application now"
-java -jar spring-petclinic*.jar > /dev/null 2> /dev/null < /dev/null &
-#java -jar spring-petclinic*.jar &
-echo "ran jar file in background"
+echo "command to stop java"
